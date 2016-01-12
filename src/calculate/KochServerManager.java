1fb48f40fec68d3e60bba505e4a51b4979b1569e
binq1000 <t.daniels@student.fontys.nl>
2016-01-12 16:03:50 +0100

    Begin KochServerManager

---
+++ src/calculate/KochServerManager.java
@@ -0,0 +1,20 @@
+package calculate;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Created by Nekkyou on 12-1-2016.
+ */
+public class KochServerManager implements Observer
+{
+    public KochServerManager() {
+        //Constructor
+    }
+
+    @Override
+    public void update(Observable observable, Object o)
+    {
+
+    }
+}
--- src/serverRunnable.java
+++ src/serverRunnable.java
@@ -1,23 +1,38 @@
 import java.io.*;
 import java.net.Socket;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 /**
  * Created by Nekkyou on 6-1-2016.
  */
 public class serverRunnable implements Runnable {
     private Socket socket = null;
+    private CalculateProtocol calculateProtocol;
 
     public serverRunnable(Socket socket) {
+
         this.socket = socket;
     }
 
     @Override
     public void run()
     {
+        calculateProtocol = new CalculateProtocol();
         try {
             ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
             HashMap<Integer, Integer> levelWithProtocol = (HashMap<Integer, Integer>) ois.readObject();
+            //Do stuff with the level and protocol
+            int level = 0;
+            int protocol = 0;
+            for (Map.Entry<Integer, Integer> entry : levelWithProtocol.entrySet()) {
+                level = entry.getKey();
+                protocol = entry.getValue();
+            }
+
+            calculateProtocol.processInput(level, protocol);
+
             ois.close();
         } catch (IOException e) {
             e.printStackTrace();
