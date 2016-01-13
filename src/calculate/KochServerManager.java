89204cfb2ab282a34ded94a70e497a4bd086b457
binq1000 <t.daniels@student.fontys.nl>
2016-01-13 09:56:55 +0100

    Latest changes, MMF

--- src/calculate/KochServerManager.java
+++ src/calculate/KochServerManager.java
@@ -1,9 +1,11 @@
 package calculate;
 
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
+import timeutil.TimeStamp;
+
+import java.io.*;
 import java.net.Socket;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
@@ -23,6 +25,10 @@ public class KochServerManager implements Observer
     private OutputStream outStream;
     private ObjectOutputStream out;
 
+    //For MAF
+    private ByteArrayOutputStream baos;
+    private ObjectOutputStream outBin;
+
 
     public KochServerManager(int protocol, int level, Socket socket) {
         System.out.println("Created a KochServerManager");
@@ -34,6 +40,9 @@ public class KochServerManager implements Observer
         {
             outStream = socket.getOutputStream();
             out = new ObjectOutputStream(outStream);
+
+            baos = new ByteArrayOutputStream();
+            outBin = new ObjectOutputStream(baos);
         }
         catch (IOException e)
         {
@@ -52,6 +61,8 @@ public class KochServerManager implements Observer
         else if(protocol == 2){
             writeEdgeForEdge();
         }
+
+        createMemMappedFile();
     }
 
     @Override
@@ -85,6 +96,7 @@ public class KochServerManager implements Observer
         try
         {
             out.writeObject(endString);
+            outBin.writeObject(edges);
         }
         catch (IOException e)
         {
@@ -109,7 +121,74 @@ public class KochServerManager implements Observer
         try
         {
             out.writeObject(edges);
+            outBin.writeObject(edges);
             out.flush();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void createMemMappedFile() {
+        byte[] bytes = null;
+
+        bytes = baos.toByteArray();
+
+        RandomAccessFile memoryMappedFile = null;
+        try
+        {
+            memoryMappedFile = new RandomAccessFile("D:\\cacheFile.txt", "rw");
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
+        //Mapping a file into memory
+        FileChannel      fc  = memoryMappedFile.getChannel();
+        MappedByteBuffer out = null;
+        try
+        {
+            out = fc.map(FileChannel.MapMode.READ_WRITE, 0, bytes.length);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        out.put(bytes);
+    }
+
+    public void readMemMappedFile() {
+        try
+        {
+            RandomAccessFile memoryMappedFile = new RandomAccessFile("D:\\cacheFile.txt", "r");
+
+            //Mapping a file into memory
+            FileChannel      fc  = memoryMappedFile.getChannel();
+            MappedByteBuffer out = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+
+            byte[] bytes = new byte[(int) fc.size()];
+
+            //reading from memory file in Java
+            out.get(bytes);
+            ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+            ObjectInputStream    ois  = new ObjectInputStream(bais);
+
+            try
+            {
+                edges = (ArrayList<Edge>) ois.readObject();
+            }
+            catch (ClassNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
         }
         catch (IOException e)
         {
